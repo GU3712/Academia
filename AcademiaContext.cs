80852ada48c6c551faa4131819446adab8f04bc1
GU3712 <gustavo.s.pereira15@aluno.senai.br>
2026-08-05 09:06:04 -0300

    AcademiaContext

--- AcademiaContext.cs
+++ AcademiaContext.cs
@@ -1,4 +1,4 @@
-﻿using Academia.Models;
+using Academia.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Reflection.Emit;
@@ -12,18 +12,24 @@ namespace Academia.Data
         public DbSet<Aluno> Alunos { get; set; }
         public DbSet<Plano> Planos { get; set; }
         public DbSet<Matricula> Matriculas { get; set; }
+        public DbSet<Pagamento> Pagamentos { get; set; }
         public DbSet<Instrutor> Instrutores { get; set; }
-        public DbSet<FichaTreino> FichasTreino { get; set; }
+        public DbSet<Treino> Treinos { get; set; }
         public DbSet<Exercicio> Exercicios { get; set; }
+        public DbSet<ItemTreino> ItensTreino { get; set; }
+        public DbSet<AvaliacaoFisica> AvaliacoesFisicas { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Aluno>().ToTable("Aluno").HasKey(a => a.IdAluno);
             modelBuilder.Entity<Plano>().ToTable("Plano").HasKey(p => p.IdPlano);
             modelBuilder.Entity<Matricula>().ToTable("Matricula").HasKey(m => m.IdMatricula);
+            modelBuilder.Entity<Pagamento>().ToTable("Pagamento").HasKey(pg => pg.IdPagamento);
             modelBuilder.Entity<Instrutor>().ToTable("Instrutor").HasKey(i => i.IdInstrutor);
-            modelBuilder.Entity<FichaTreino>().ToTable("Ficha_Treino").HasKey(f => f.IdTreino);
+            modelBuilder.Entity<Treino>().ToTable("Treino").HasKey(t => t.IdTreino);
             modelBuilder.Entity<Exercicio>().ToTable("Exercicio").HasKey(e => e.IdExercicio);
+            modelBuilder.Entity<ItemTreino>().ToTable("Item_Treino").HasKey(it => it.IdItemTreino);
+            modelBuilder.Entity<AvaliacaoFisica>().ToTable("Avaliacao_Fisica").HasKey(af => af.IdAvaliacao);
         }
     }
 }
