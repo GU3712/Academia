32cbd959860c5465e70a56416941c4db656d7005
GU3712 <gustavo.s.pereira15@aluno.senai.br>
2026-08-05 09:07:45 -0300

    Refactor Aluno class with new properties and validations
    Added Email and Status properties with validation attributes.

--- Aluno.cs
+++ Aluno.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace AcademiaApp.Models
+namespace Academia.Models
 {
     public class Aluno
     {
@@ -8,9 +8,11 @@ namespace AcademiaApp.Models
         public int IdAluno { get; set; }
 
         [Required(ErrorMessage = "Informe o nome.")]
+        [StringLength(100)]
         public string Nome { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Informe o CPF.")]
+        [StringLength(14)]
         public string Cpf { get; set; } = string.Empty;
 
         [DataType(DataType.Date)]
@@ -18,8 +20,14 @@ namespace AcademiaApp.Models
 
         public string? Telefone { get; set; }
 
-        
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
+        public string? Email { get; set; }
+
+        [Required]
+        public string Status { get; set; } = "Ativo";
+
         public ICollection<Matricula>? Matriculas { get; set; }
-        public ICollection<FichaTreino>? FichasTreino { get; set; }
+        public ICollection<Treino>? Treinos { get; set; }
+        public ICollection<AvaliacaoFisica>? AvaliacoesFisicas { get; set; }
     }
 }
